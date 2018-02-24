b3b594a7f3e4ff4f6426a58bfd953ebd062dfab0
peanutDD <www.smoking.sexy@gmail.com>
2018-02-24 21:27:16 +0800

    change App.js Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,25 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age : props.initialAge
+    };
+  }
+
+  onMakeOlder() {
+    this.setState({
+      age: this.state.age + 5
+    });
+  }
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-xs-1 col-xs-offset-11">
-            <div>your name is {this.props.name}, your age is {this.props.age}</div>
+            <div>your name is {this.props.name}, your age is {this.state.age}</div>
+            <button onClick={() => {this.onMakeOlder()}} class="btn btn-primary">make me older</button>
             <div>
               <h4>hobbies</h4>
               <ul>
